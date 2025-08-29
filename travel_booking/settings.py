05959556d859ec4706cb4db26fcfd8dfe7ce078c
Divya B Gowda <117468949+DivyaBGowda484@users.noreply.github.com>
2025-08-30 00:11:02 +0530

    deployment chnages

--- travel_booking/settings.py
+++ travel_booking/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ezzbyowst!=)f$qt-=ja5d-6c4g9k#c7hfbdscj79ml9b+ek-0'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ['*']
 
